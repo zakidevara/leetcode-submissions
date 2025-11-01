e6428a52ad34015c1e17dc27ea7ba47ac86acbd8
Zaki Devara <muhammad.devara@traveloka.com>
2025-11-01 09:32:05 +0000

    [LeetCode Sync] Runtime - 4 ms (43.82%), Memory - 45.2 MB (92.28%)

---
+++ my-folder/0120-triangle/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.getLast().size();
+        int[] dp = new int[n];
+
+        // search min path from the bottom
+        for (int i = 0; i < n; i++) {
+            dp[i] = triangle.get(n-1).get(i);
+        }
+
+        for (int i = n-2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                dp[j] = triangle.get(i).get(j) +  Math.min(dp[j], dp[j+1]);
+            }
+        }
+
+        return dp[0];
+    }
+}
