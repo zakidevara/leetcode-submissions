7ff677a882600b6bce979556719586e1df82702b
Zaki Devara <muhammad.devara@traveloka.com>
2026-05-02 16:55:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.9 MB (16.63%)

---
+++ my-folder/0048-rotate-image/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // transpose
+        for (int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // reverse each row
+        for (int i = 0; i < n; i++) {
+            int l = 0;
+            int r = n - 1;
+
+            while (l < r) {
+                int temp = matrix[i][l];
+                matrix[i][l] = matrix[i][r];
+                matrix[i][r] = temp;
+                l++;
+                r--;
+            }
+
+        }
+    }
+}
+
