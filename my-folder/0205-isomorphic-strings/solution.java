df8643aabf3a5d48c121b32f86189abf85e23555
Zaki Devara <muhammad.devara@traveloka.com>
2026-05-21 07:48:47 +0000

    [LeetCode Sync] Runtime - 18 ms (25.37%), Memory - 43.9 MB (59.22%)

---
+++ my-folder/0205-isomorphic-strings/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Integer> charIndexS = new HashMap<>();
+        HashMap<Character, Integer> charIndexT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!charIndexS.containsKey(s.charAt(i))) {
+                charIndexS.put(s.charAt(i), i);
+            }
+
+            if (!charIndexT.containsKey(t.charAt(i))) {
+                charIndexT.put(t.charAt(i), i);
+            }
+
+            if (!charIndexS.get(s.charAt(i)).equals(charIndexT.get(t.charAt(i)))) {
+                return false;
+            }
+        }
+
+        return true;        
+    }
+}
