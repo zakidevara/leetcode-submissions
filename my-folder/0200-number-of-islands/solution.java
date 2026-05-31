f553e81c3ad02ad822f805ac7b61ff5d34016b7f
Zaki Devara <muhammad.devara@traveloka.com>
2026-05-31 10:32:41 +0000

    [LeetCode Sync] Runtime - 3 ms (87.98%), Memory - 52.2 MB (68.69%)

--- my-folder/0200-number-of-islands/solution.java
+++ my-folder/0200-number-of-islands/solution.java
@@ -3,28 +3,28 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        int result = 0;
+        int counter = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                char currVal = grid[i][j];
-                if (currVal == '1') {
-                    result++;
-                    dfs(grid, i, j);
-                }
+                if (grid[i][j] == '0') continue;
+
+                counter++;
+                dfs(grid, i, j);
             }
         }
 
-        return result;
+        return counter;
     }
 
-    private void dfs(char[][]grid, int i, int j) {
-        if (i >= grid.length || j >= grid[0].length || i < 0 || j < 0 || grid[i][j] != '1') return;
+
+    public void dfs(char[][] grid, int i, int j) {
+        if (i >= grid.length || j >= grid[0].length || i < 0 || j < 0 || grid[i][j] == '0') return;
 
         grid[i][j] = '0';
+
         dfs(grid, i+1, j);
         dfs(grid, i-1, j);
         dfs(grid, i, j+1);
         dfs(grid, i, j-1);
-        
     }
 }
