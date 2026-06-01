e474ee58f2cab07a116e99f215b6dfb46faf9dfe
Zaki Devara <muhammad.devara@traveloka.com>
2026-06-01 03:34:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (54.78%)

--- my-folder/0070-climbing-stairs/solution.java
+++ my-folder/0070-climbing-stairs/solution.java
@@ -1,15 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-        return recursive(n, new HashMap());
-    }
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int steps1 = 1;
+        int steps2 = 2;
 
-    private int recursive(int n, Map<Integer, Integer> dp) {
-        if (n == 0) return 1;
-        if (n < 0) return 0;
-        if (dp.get(n) != null) return dp.get(n);
+        for (int i = 3; i <= n; i++) {
+            int curr = steps2 + steps1;
 
-        int result = recursive(n - 2, dp) + recursive(n - 1, dp);
-        dp.put(n, result);
-        return result;
+            steps1 = steps2;
+            steps2 = curr;
+        }
+
+        return steps2;
     }
 }
