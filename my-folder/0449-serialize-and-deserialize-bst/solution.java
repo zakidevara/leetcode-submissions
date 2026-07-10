26189af2d23c4bad27e6a44c8b0b75cde2748bac
Zaki Devara <muhammad.devara@traveloka.com>
2026-07-10 18:59:10 +0000

    [LeetCode Sync] Runtime - 7 ms (46.40%), Memory - 47.4 MB (41.97%)

---
+++ my-folder/0449-serialize-and-deserialize-bst/solution.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        preorderSerialize(root, sb);
+
+        return sb.toString();
+    }
+
+    private void preorderSerialize(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append("x,");
+            return;
+        }
+
+        sb.append(root.val).append(",");
+
+        preorderSerialize(root.left, sb);
+        preorderSerialize(root.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] arr = data.split(",");
+
+        Queue<String> q = new LinkedList<>();
+
+        q.addAll(Arrays.asList(arr));
+
+        return preorderDeserialize(q);
+    }
+
+    private TreeNode preorderDeserialize(Queue<String> q) {
+        if (q.isEmpty()) return null;
+        String curr = q.poll();
+
+        if (curr.equals("x")) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(curr));
+
+        root.left = preorderDeserialize(q);
+        root.right = preorderDeserialize(q);
+
+        return root;
+
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// String tree = ser.serialize(root);
+// TreeNode ans = deser.deserialize(tree);
+// return ans;
