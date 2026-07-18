0ace6c11fabc74df60df697fc8e6ca73ff4fa737
Zaki Devara <muhammad.devara@traveloka.com>
2026-07-18 08:49:58 +0000

    [LeetCode Sync] Runtime - 34 ms (50.69%), Memory - 100 MB (43.19%)

--- my-folder/0155-min-stack/solution.java
+++ my-folder/0155-min-stack/solution.java
@@ -1,44 +1,41 @@
 class MinStack {
-
-    private List<Integer> stack = new ArrayList<>(); // store min val of each push step interweavingly
+    private Stack<Integer> mainStack;
+    private Stack<Integer> minStack;
 
     public MinStack() {
+        mainStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int value) {
+        mainStack.push(value);
+        
+        if (minStack.isEmpty() || minStack.peek() >= value) {
+            minStack.push(value);
+        }
         
     }
     
-    public void push(int val) {
-       stack.add(val);
-       if (stack.size() == 1) {
-        stack.add(val); // add itself as min val
-       } else {
-        int prevMinVal = stack.get(stack.size() - 2);
-        if (val < prevMinVal) {
-            stack.add(val);
-        } else {
-            stack.add(prevMinVal);
-        }
-       }
-    }
-    
     public void pop() {
-        // remove the min val item & top stack item
-        stack.remove(stack.size()-1); // min val
-        stack.remove(stack.size()-1); // top stack
+        if (!minStack.isEmpty() && mainStack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        mainStack.pop();
     }
     
     public int top() {
-        return stack.get(stack.size()-2);
+        return mainStack.peek();
     }
     
     public int getMin() {
-        return stack.getLast();
+        return minStack.peek();
     }
 }
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
- * obj.push(val);
+ * obj.push(value);
  * obj.pop();
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
