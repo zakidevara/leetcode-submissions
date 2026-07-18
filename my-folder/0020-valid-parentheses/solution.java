425e51aa08db7d7c742df84415e0cef76c06adea
Zaki Devara <muhammad.devara@traveloka.com>
2026-07-18 08:10:23 +0000

    [LeetCode Sync] Runtime - 4 ms (38.09%), Memory - 43.1 MB (73.56%)

--- my-folder/0020-valid-parentheses/solution.java
+++ my-folder/0020-valid-parentheses/solution.java
@@ -1,29 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> findStack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char currChar = s.charAt(i);
-            Character complement = getComplementBracket(currChar);
-            if (complement != null) {
-                findStack.push(complement);
-            } else if (findStack.isEmpty()) {
-                return false;
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> brackets = Map.of(
+            '(', ')',
+            '[', ']',
+            '{', '}'
+        );
+        for (Character c : s.toCharArray()) {
+            if (brackets.containsKey(c)) {
+                stack.push(brackets.get(c));
+            } else if (!stack.isEmpty() && c == stack.peek()) {
+                stack.pop();
             } else {
-                Character target = findStack.pop();
-                if (!target.equals(currChar)) return false;
+                return false;
             }
         }
-
-        return findStack.isEmpty();
+        
+        return stack.isEmpty();
     }
-
-    private Character getComplementBracket(Character c) {
-        switch (c) {
-            case '(': return ')';
-            case '{': return '}';
-            case '[': return ']';
-        }
-        return null;
-    }
+    
 }
