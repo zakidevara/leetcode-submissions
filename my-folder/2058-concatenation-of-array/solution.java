6d207327279f61714d37e4761a23bd2192144df5
Zaki Devara <muhammad.devara@traveloka.com>
2026-07-25 06:39:52 +0000

    [LeetCode Sync] Runtime - 1 ms (97.54%), Memory - 47 MB (90.27%)

---
+++ my-folder/2058-concatenation-of-array/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] result = new int[nums.length*2];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = result[nums.length+i] = nums[i];
+        }
+
+        return result;
+    }
+}
