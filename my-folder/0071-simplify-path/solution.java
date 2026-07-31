aeabb0b3577423f4dabb96ec9346f0fbd5665bdf
Zaki Devara <muhammad.devara@traveloka.com>
2026-07-31 04:22:22 +0000

    [LeetCode Sync] Runtime - 5 ms (51.69%), Memory - 45.1 MB (27.90%)

--- my-folder/0071-simplify-path/solution.java
+++ my-folder/0071-simplify-path/solution.java
@@ -1,33 +1,35 @@
 class Solution {
     public String simplifyPath(String path) {
-        List<String> stack = new ArrayList<>();
-        int previousSlashIdx = 0;
+        Stack<String> stack = new Stack<>();
 
-        if (!path.endsWith("/")) path += "/";
-
-        for (int i = 1; i < path.length(); i++) {
-            if (path.charAt(i) == '/') {
-
-                String token = path.substring(previousSlashIdx+1, i);
-                if (i - previousSlashIdx == 1) {
-                    // do nothing
-                } else if ("..".equals(token)) {
-                    if (stack.size() > 0) stack.remove(stack.size()-1);
-                } else if (!".".equals(token)){
-                    stack.add(path.substring(previousSlashIdx+1, i));
-                }
-                previousSlashIdx = i;
+        int left = 0;
+        int right = 0;
+        while (right < path.length() && left < path.length()) {
+            while (left < path.length() && path.charAt(left) == '/') {
+                left++;
             }
+            right = left;
+
+            while (right < path.length() && path.charAt(right) != '/') {
+                right++;
+            }
+
+            String currDir = path.substring(left, right);
+
+            if (currDir.equals("..")) {
+                if (!stack.isEmpty()) stack.pop();
+            } else if (!currDir.isEmpty() && !currDir.equals(".")) {
+                stack.push(currDir);
+            }
+            left = right + 1;
         }
 
-        return constructPath(stack);
-    }
-
-    private String constructPath(List<String> stack) {
-        String result = "";
-        for (int i = 0; i < stack.size(); i++) {
-            if (stack.get(i) != null) result += "/" + stack.get(i);
+        List<String> list = new LinkedList<>();
+        while (!stack.isEmpty()) {
+            list.addFirst(stack.pop());
         }
-        return result.isEmpty() ? "/" : result;
+
+
+        return "/" + String.join("/", list);
     }
 }
